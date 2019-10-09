f40a3766dea5b2471ed3165d568e0b6e523393a6
Nhung Hoang <nhunghoang@Nhungs-MacBook-Pro.local>
2019-10-10 08:40:15 +1100

    show welcome message when start the application

---
+++ test/com/twu/biblioteca/Biblioteca.java
@@ -0,0 +1,14 @@
+package com.twu.biblioteca;
+
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class Biblioteca {
+
+    @Test
+    public void BibliotecaApp() {
+        assertEquals( "Welcome to Biblioteca. Your one-stop-shop for great book tittles in Bangalore!", "Welcome to Biblioteca. Your one-stop-shop for great book tittles in Bangalore!");
+
+    }
+}
