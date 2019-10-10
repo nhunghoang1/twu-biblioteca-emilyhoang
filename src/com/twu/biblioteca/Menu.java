3b111171fa3ccda9cc5a6ccfdbcb65c569085fa3
EmilyHoang <nhung@nhunghoang.com>
2019-10-11 10:39:38 +1100

    add notification when successfully check out a book

--- src/com/twu/biblioteca/Menu.java
+++ src/com/twu/biblioteca/Menu.java
@@ -1,9 +1,12 @@
 package com.twu.biblioteca;
 
+import java.util.Scanner;
+
 public class Menu {
     public static void reader() {
         System.out.println("Please choose an option!");
         System.out.println("1 - List of books");
+        System.out.println("2 - Check out a book");
         System.out.println("0 - Quit");
     }
 
@@ -11,7 +14,14 @@ public class Menu {
         if (line == 1) {
             BookList bookList = new BookList();
             bookList.printBookList();
-        }else if (line == 0) {
+        }else if (line == 2) {
+            Scanner myObj = new Scanner(System.in);
+            System.out.println("Please enter a book tittle");
+
+            String userInput = myObj.nextLine();
+            System.out.println("Thank you! Enjoy the book");
+        }
+        else if (line == 0) {
             // the application will quit
         }
         else {
